ec62ebe01f3f29e5ffbb7a0c4a8292192cc2470d
SabarisshanGK <sabarivisva007@gmail.com>
2024-03-12 09:28:09 +0530

    Implemented ArrayList Succesfully

---
+++ src/ArrayListImplementation.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListImplementation {
+    // Main Function
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        arr.add(1);
+        arr.add(25);
+
+        System.out.println(arr);
+    }
+}
