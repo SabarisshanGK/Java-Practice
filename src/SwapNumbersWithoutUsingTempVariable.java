abc58ec456b47830fb380af3c36e7397cc73cec4
SabarisshanGK <sabarivisva007@gmail.com>
2024-03-14 18:02:25 +0530

    COmpleted swap numbers without using Temporary Variable

---
+++ src/SwapNumbersWithoutUsingTempVariable.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SwapNumbersWithoutUsingTempVariable {
+    // Main Function
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        // Get Value of number1 from User
+        System.out.print("Enter A:\t");
+        int a = input.nextInt();
+
+        // Get Value of number2 from User
+        System.out.print("Enter B:\t");
+        int b = input.nextInt();
+
+        System.out.println("Numbers before swapping: "+a+" , "+b);
+
+        a=  a + b;
+        b=  a- b;
+        a= a-b;
+
+        System.out.println("Numbers after swapping: "+a+" , "+b);
+
+    }
+}
